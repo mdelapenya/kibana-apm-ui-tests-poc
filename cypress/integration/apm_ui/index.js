a56fc34c7bee34583d37a1ff5b38a4edc9900e28
Manuel de la Peña <mdelapenya@gmail.com>
2019-07-23 13:30:49 +0200

    Add a first test using test containers

--- cypress/integration/apm_ui/index.js
+++ cypress/integration/apm_ui/index.js
@@ -1,19 +1,41 @@
-/* global And, Given, Then */
+/* global And, Given, Then, cy */
 
-const environment = Cypress.env("testEnvironment");
+const { GenericContainer } = require("testcontainers");
 
 Cypress.Screenshot.defaults({
   screenshotOnRunFailure: true
 });
 
-Given(`the Elastic Stack in {string} is {string}`, (version, status) => {
-  console.log("The Elastic Stack " + version + " is " + status);
+var elasticSearchContainer;
+var kibanaContainer;
+
+async function runElasticStack(version) {
+  elasticSearchContainer = await new GenericContainer("docker.elastic.co/elasticsearch/elasticsearch", version)
+    .withExposedPorts(9200)
+    .start();
+  kibanaContainer = await new GenericContainer("docker.elastic.co/kibana/kibana", version)
+    .withExposedPorts(9200)
+    .start();
+}
+
+async function stopElasticStack() {
+  await kibanaContainer.stop();
+  await elasticSearchContainer.stop();
+}
+
+Given(`the Elastic Stack in {string} is {string}`, async(version, status) => {
+  await runElasticStack(version);
+  cy.log("The Elastic Stack " + version + " is " + status);
 });
 
-When(`the user {string} logs in`, userName => {
-  console.log("User " + userName + " is logged in");
+When(`the user {string} logs in`, async(userName) => {
+  cy.log("User " + userName + " is logged in");
+  setTimeout(function() {
+    cy.log('Sleeping for 5 seconds');
+  }, 5000);
 });
 
-Then(`Kibana's main menu displays the {string} icon`, moduleName => {
-  console.log("Module " + moduleName + " is not visible");
+Then(`Kibana's main menu displays the {string} icon`, async(moduleName) => {
+  cy.log("Module " + moduleName + " is not visible");
+  await stopElasticStack();
 });
